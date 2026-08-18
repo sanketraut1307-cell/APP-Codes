9c0a20094605ec01dc7a0f2e4c72c91889954d4c
Sanket Raut <sanketraut1307@gmail.com>
2026-08-18 11:33:22 +0530

    Create Writing.py

---
+++ File_Handling_Basics/Writing.py
@@ -0,0 +1,14 @@
+# Write data into a file
+
+file = open("data.txt", "w")
+
+file.write("Hello, this is my first file.\n")
+file.write("I am learning Python file handling.")
+
+file.close()
+
+print("Data written successfully.")
+
+# Output - 
+# Hello, this is my first file.
+# I am learning Python file handling.
